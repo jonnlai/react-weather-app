e4a8d3dc95d1df7c3430ac1e1c0e61fd0ed53aaa
jonnlai <121501241+jonnlai@users.noreply.github.com>
2025-02-16 18:05:00 +0000

    add footer

--- src/App.js
+++ src/App.js
@@ -11,6 +11,16 @@ function App() {
         <Search />
         <Weather />
         <Update />
+        <div class="footer">
+          <a
+            href="https://github.com/jonnlai/react-weather-app"
+            target="_blank"
+            rel="noreferrer noopener"
+          >
+            Open-source code
+          </a>{" "}
+          by Jonna Laine.
+        </div>
       </div>
     </div>
   );
